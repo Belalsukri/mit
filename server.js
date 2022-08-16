796e492e8cd84ce21ad77d3dcee758dec1254876
Belal Sukari <belalsukari@gmail.com>
2022-08-17 00:23:11 +0200

    make 545

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const path = require('path');
 const app =express()
 const port = process.env.PORT || 5000
 
-app.use(express.static('build'));
-app.get('*', function (req, res) {
-  res.sendFile('index.html');
+
+
+app.use(express.static(path.join(__dirname ,'client/build')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname ,'client/build', 'index.html'));
 });
-// app.use(express.static(path.join(__dirname ,'client' ,'build')));
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname ,'client','build', 'index.html'));
-// });
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
