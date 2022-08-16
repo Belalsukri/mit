398e3a26e7916c5efddd8191838b89e776b5a1e4
Belal Sukari <belalsukari@gmail.com>
2022-08-16 23:53:15 +0200

    make it

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const path = require('path');
-const server =express()
+const app =express()
 const port = process.env.PORT || 5000
 
 
 
-server.use(express.static(path.join(__dirname ,".", 'client' ,'build')));
-server.get('*', function (req, res) {
+app.use(express.static(path.join(__dirname ,".", 'client' ,'build')));
+app.get('*/', function (req, res) {
   res.sendFile(path.join(__dirname,"." ,'client','build', 'index.html'));
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
 });
