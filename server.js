04ac8a5d51f259880f45df4c4098afa8565950ef
Belal Sukari <belalsukari@gmail.com>
2022-08-17 21:09:10 +0200

    make it better

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const path = require('path');
-const app =express()
+
 const port = process.env.PORT || 5000
-
-
-
-app.use(express.static(path.join(__dirname ,'client/build')));
+const app =express()
+app.use(express.static(path.join(__dirname ,'/client/public')));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname ,'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname ,'/client/public', 'index.html'));
 });
 
 app.listen(port, () => {
