317691c2c37ad670445b1a78a1d91c388d62d07e
Belal Sukari <belalsukari@gmail.com>
2022-08-17 21:53:32 +0200

    make it bejk2

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const port = process.env.PORT || 5000
 const app =express()
-app.use(express.static(path.join(__dirname ,'../','client','build')));
+app.use(express.static(path.join(__dirname ,'client','build')));
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname ,'../','client','build', 'index.html'));
+  res.sendFile(path.join(__dirname ,'client','build', 'index.html'));
 });
 
 app.listen(port, () => {
